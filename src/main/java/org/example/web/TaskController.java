032db4730995d2de86266109803792eff18eef15
quickestfunctioning <snc01004@naver.com>
2024-11-22 11:58:29 +0900

    새로운 할 일 추가 API 구현

--- src/main/java/org/example/web/TaskController.java
+++ src/main/java/org/example/web/TaskController.java
@@ -23,23 +23,12 @@ public class TaskController {
 
     private final TaskService taskService;
 
-    /**
-     * 새로운 할 일 추가
-     * @param req 추가하고자 하는 할 일
-     * @return 추가된 할 일
-     */
     @PostMapping
     public ResponseEntity<Task> createTask(@RequestBody TaskRequest req) {
         var result = this.taskService.add(req.getTitle(), req.getDescription(), req.getDueDate());
         return ResponseEntity.ok(result);
     }
 
-    /**
-     * 특정 마감일에 해당하는 할일 목록을 반환
-     *
-     * @param dueDate 할일의 마감일
-     * @return 마감일에 해당하는 할일 목록
-     */
     @GetMapping
     public ResponseEntity<List<Task>> getTask(Optional<String> dueDate) {
         List<Task> result;
@@ -53,37 +42,18 @@ public class TaskController {
         return ResponseEntity.ok(result);
     }
 
-    /**
-     * 특정 ID에 해당하는 할일을 조회
-     *
-     * @param id 할일 ID
-     * @return ID에 해당하는 할일 객체
-     */
     @GetMapping("/{id}")
     public ResponseEntity<Task> fetchOneTask(@PathVariable Long id) {
         var result = this.taskService.getOne(id);
         return ResponseEntity.ok(result);
     }
 
-    /**
-     * 특정 상태에 해당하는 할일 목록을 반환
-     *
-     * @param status 할일 상태
-     * @return 상태에 해당하는 할일 목록
-     */
     @GetMapping("/status/{status}")
     public ResponseEntity<List<Task>> getByStatus(@PathVariable TaskStatus status) {
         var result = this.taskService.getByStatus(status);
         return ResponseEntity.ok(result);
     }
 
-    /**
-     * 특정 ID에 해당하는 할일을 수정
-     *
-     * @param id 할일 ID
-     * @param task 수정할 할일 정보
-     * @return 수정된 할일 객체
-     */
     @PutMapping("/{id}")
     public ResponseEntity<Task> updateTask(@PathVariable Long id,
                                            @RequestBody TaskRequest task) {
@@ -94,13 +64,6 @@ public class TaskController {
         return ResponseEntity.ok(result);
     }
 
-    /**
-     * 특정 ID에 해당하는 할일의 상태를 수정
-     *
-     * @param id 할일 ID
-     * @param req 수정할 할일 상태 정보
-     * @return 수정된 할일 객체
-     */
     @PatchMapping("/{id}/status")
     public ResponseEntity<Task> updateTaskStatus(@PathVariable Long id,
                                                  @RequestBody TaskStatusRequest req) {
@@ -108,12 +71,6 @@ public class TaskController {
         return ResponseEntity.ok(result);
     }
 
-    /**
-     * 특정 ID에 해당하는 할일을 삭제
-     *
-     * @param id 삭제할 할일 ID
-     * @return 삭제 결과를 담은 응답 객체
-     */
     @DeleteMapping("/{id}")
     public ResponseEntity<ResultResponse> deleteTask(@PathVariable Long id) {
         var result = this.taskService.delete(id);
